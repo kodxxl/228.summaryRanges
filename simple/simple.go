ef9a87fcd8ccc4045a336a881774dbf347f6055c
kodxxl <xsrc@mail.ru>
2024-10-24 09:51:55 +0400

    With snapshot function

--- simple/simple.go
+++ simple/simple.go
@@ -1,33 +1,28 @@
 package simple
 
-import "strconv"
+import "fmt"
 
 func SummaryRanges(nums []int) []string {
 	result := []string{}
 	startCursor, endCursor := 0, 0
-	lenNums := len(nums)
+	lastIndex := len(nums) - 1
 
 	for i := range nums {
 		endCursor = i
-		if i < (lenNums - 1) {
+		if i < lastIndex {
 			if (nums[i] + 1) == nums[i+1] {
 				continue
 			}
 		}
-		annotation := makeAnnotation(nums, startCursor, endCursor)
-		result = append(result, annotation)
+		result = append(result, snapshot(nums, startCursor, endCursor))
 		startCursor = (i + 1)
 	}
-
 	return result
 }
 
-func makeAnnotation(nums []int, startCursor, endCursor int) string {
-	first := nums[startCursor]
-	last := nums[endCursor]
-
+func snapshot(nums []int, startCursor, endCursor int) string {
 	if startCursor == endCursor {
-		return strconv.Itoa(first)
+		return fmt.Sprintf("%d", nums[startCursor])
 	}
-	return strconv.Itoa(first) + "->" + strconv.Itoa(last)
+	return fmt.Sprintf("%d->%d", nums[startCursor], nums[endCursor])
 }
